3782ff5b78d20dd8258c9916d699f015957bb204
Jamie Martin <james.campbell.martin@gmail.com>
2024-02-13 02:44:12 -0500

    commit

---
+++ col_names.py
@@ -0,0 +1,16 @@
+# optional but recommended style to include a comma 
+# after the final element in a list
+column_names = [
+    'ClassFilter',
+    'Credit Type',
+    'Curriculum.Course Pathway',
+    'Curriculum.Credit Non Credit Vocational',
+    'Curriculum.Primary Instructor Id',
+    'Curriculum.Primary Instructor Type',
+    'Department',
+    'FTE',
+    'Gen Ed',
+    'Paid FTE',
+    'Random',
+    'SummaryKey',
+]  
